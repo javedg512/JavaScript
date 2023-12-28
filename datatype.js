4c717da909e0c8a728dcae6e933c36270a576d8a
Javed Gauri <110252831+javedg512@users.noreply.github.com>
2023-12-28 09:26:06 +0000

    DataTypes

---
+++ datatype.js
@@ -0,0 +1,19 @@
+let nam= "Javed ";
+let number = 15;
+let stat = true;
+let val = null;
+let num;
+console.table([nam,number,stat,val,num,typeof(nam)]);
+console.log(typeof(num))
+console.log(typeof(val));
+//alert("hello") it will not gonna execute because we are running this in cmd not in browser
+//prompt("Enter a number "); same with this 
+
+/* number 2 pow 53
+ big int 
+ String ""
+ Boolean true/false
+ null = jab value khali rakhni ho or undefined nahi chahhiye ho // standalone value 
+ undefined jab variable me koi value na ho
+ 
+ */
